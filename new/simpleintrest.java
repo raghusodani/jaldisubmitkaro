0aa2996dc5140eeb01a6a5e171bf7104e1b5119e
Dev Parmar <devparmar37@gmail.com>
2020-10-03 14:20:45 +0530

    add this

---
+++ new/simpleintrest.java
@@ -0,0 +1,14 @@
+package Lab;
+//**Parmar Dev
+//**201951110
+
+public class simpleintrest {
+	public static void main(String args[]) {
+		float p,r,t,si;
+		p=1200;
+		r=2;
+		t=12;
+		si=(p*r*t)/100;
+		System.out.println("simple interest is:"+si);
+	}
+}
